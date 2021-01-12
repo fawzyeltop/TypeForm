ca274fea323184afc7e59a64adc6c43db3d0a80b
Your Name <you@example.com>
2021-01-12 19:58:54 +0200

    add all files

--- index.js
+++ index.js
@@ -6,14 +6,63 @@ $(() => {
     }
 
     const initJSDOM = () => {
-        const start = $("#start"), hiThere = $("#hi-there"), la = $("#la"), phoneSection = $("#phone-section"), Statement = $("#Statement"), yesNo = $("#yesNo");
-        const shortText = $("#shortText-section"), longText = $("#longText-section");
-        start.click(() => {
-            hiThere.addClass("d-none");
-            la.addClass("d-block animate__animated animate__slideInUp");
+        var counter = 1;
+        // Start Button
+        const start = $("#start");
+        start.click((e) => {
+            const ele = e.target.parentElement.parentElement.parentElement;
+            $(ele).addClass("d-noneN");
+            $(ele).next().addClass("d-block animate__animated animate__slideInUp");
+            $(".footer-pw").addClass('d-flex animate__animated animate__fadeIn');
+        });
+       
+         // Ok Button
+         $(".ok").click((e) => {
+            const ele = e.target.parentElement.parentElement.parentElement.parentElement.parentElement;
+            $(ele).addClass("d-noneN");
+            $(ele).next().addClass("d-block animate__animated animate__slideInUp");
+            if(!$(e.target).hasClass("noCounter")) {
+                counter++;
+                $("#changeNum").text(`${ counter }`);
+                $("#progress").attr('style', `width: ${ counter * 20 }%`);
+            }
+        });
+
+        // Submit Button
+        const submit = $("#submit");
+        submit.click((e) => {
+            const ele = e.target.parentElement.parentElement.parentElement.parentElement.parentElement;
+            $(ele).addClass("d-noneN");
+            $(ele).next().addClass("d-flex animate__animated animate__slideInUp");
+            $(".footer-pw").addClass('animate__animated animate__fadeOut');
+            $(".footer-create").addClass('d-flex animate__animated animate__fadeIn');
+        });
+
+        // goUp & goDown
+        const goUp = $("#goUp"), goDown = $("#goDown");
+        goDown.click(() => {
+            const ele = $(".d-block");
+            $(ele).addClass("d-noneN");
+            $(ele).next().addClass("d-block animate__animated animate__slideInUp");
+        });
+        goUp.click(() => {
+            const ele = $(".d-block");
+            $(ele).addClass("d-noneN");
+            $(ele).prev().addClass("d-blockN animate__animated animate__slideInUp");
+        });
+
+        // goTo URL method
+        const visitURL = () => {
+            window.open('https://www.typeform.com', '_blank');
+        }
+
+        $(".create").click(() => {
+            visitURL();
         })
+
+       
+
         const globalHeaderUlLi = $(".global-header-ul-li");
-        const OK1 = $("#1-Ok"), phoneOK = $("#phoneOK"), OK3 = $("#3-Ok"), longTxtOK = $("#longTxtOK");
         globalHeaderUlLi.mouseover((e) => {
             $(e.target).find(".global-header-ul-li-span-key").addClass("d-block");
         })
@@ -21,34 +70,23 @@ $(() => {
             $(e.target).find(".global-header-ul-li-span-key").removeClass("d-block");
         })
         globalHeaderUlLi.click((e) => {
+            if($(e.target.parentElement.parentElement).hasClass("yesNo")) counter++;
+            counter--;
             globalHeaderUlLi.removeClass("animate__animated animate__bounceIn global-header-ul-li-required");
             $(e.target).addClass("animate__animated animate__bounceIn global-header-ul-li-required");
             $(e.target.parentElement).children().last().addClass("d-block");
+            counter++;
+            $("#changeNum").text(`${ counter }`);
+            $("#progress").attr('style', `width: ${ counter * 20 }%`);
         });
-        OK1.click(() => {
-            la.addClass("d-noneN");
-            phoneSection.addClass("d-block animate__animated animate__slideInUp");
-        })
-        phoneOK.click(() => {
-            phoneSection.addClass("d-noneN");
-            shortText.addClass("d-block animate__animated animate__slideInUp");
-        })
-        $(".global-header-ul-li-input").click((e) => {
-            $(e.target).addClass("changeInputClass");
-        })
+        // $(".global-header-ul-li-input").click((e) => {
+        //     $(e.target).addClass("changeInputClass");
+        // })
         $(".global-header-ul-li-input").keyup((e) => {
-            $(e.target.parentElement.parentElement).children().last().addClass("animate__animated animate__lightSpeedInLeft d-block");
+            $(e.target.parentElement.parentElement).children().last().addClass("d-block");
         })
         $(".global-header-ul-li-txtArea").keyup((e) => {
-            $(e.target.parentElement.parentElement).children().last().addClass("animate__animated animate__lightSpeedInLeft d-block");
-        })
-        OK3.click(() => {
-            shortText.addClass("d-noneN");
-            longText.addClass("d-block animate__animated animate__slideInUp");
-        })
-        longTxtOK.click(() => {
-            longText.addClass("d-noneN");
-            Statement.addClass("d-block animate__animated animate__slideInUp");
+            $(e.target.parentElement.parentElement).children().last().addClass("d-block");
         })
     }
     const initPhone = () => {
