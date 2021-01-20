9e6d13b5269e0c0f7df36a234ded8003fae2407c
Your Name <you@example.com>
2021-01-20 22:47:44 +0200

    add all files now eng

--- index.js
+++ index.js
@@ -10,69 +10,92 @@ $(() => {
 
         const counterFunc = () => {
             counter++;
-            $("#changeNum").text(`${ counter }`);
-            $("#progress").attr('style', `width: ${ counter * 20 }%`);
+            $("#changeNum").text(`${counter}`);
+            $("#progress").attr('style', `width: ${counter * 20}%`);
         }
-   
-            
 
-         $(".ok").click(() => {
-            if($("body").find('.enter').hasClass("startSection")) {
+
+
+        $(".ok").click(() => {
+            if ($("body").find('.enter').hasClass("startSection")) {
                 $(".footer-pw").addClass('d-flex animate__animated animate__fadeIn');
             }
-            if($("body").find('.enter').hasClass("yesNo")) {
+            if ($("body").find('.enter').hasClass("yesNo")) {
                 $(".footer-pw").addClass('animate__animated animate__fadeOut');
                 $(".footer-create").addClass('d-flex animate__animated animate__fadeIn');
-           }
-           if($("body").find('.enter').hasClass("la") || $("body").find('.enter').hasClass("yesNo")) {
-            if($("body").find('.enter').find(".global-header-ul-li-required").length > 0) {
-                counterFunc();
-            } 
-        }
-          
-           if($("body").find('.enter').hasClass("submit")) return visitURL();
-            if($("body").find('.enter').hasClass("phoneSection")) {
-                if($("body").find('.enter').find('#phone').val().length > 0) counterFunc();
             }
-            if($("body").find('.enter').hasClass("shortText")) {
-                if($("body").find('.enter').find('#short').val().length > 0)  counterFunc();
+            if ($("body").find('.enter').hasClass("pictureChoice") || $("body").find('.enter').hasClass("la") || $("body").find('.enter').hasClass("yesNo")) {
+                if ($("body").find('.enter').find(".global-header-ul-li-required").length > 0) {
+                    counterFunc();
+                }
             }
-            if($("body").find('.enter').hasClass("longText")) {
-                if($("body").find('.enter').find('#long').val().length > 0)  counterFunc();
+            if ($("body").find('.enter').hasClass("opinionScale")) {
+                if ($("body").find('.enter').find(".opinion-sacle-span-required").length > 0) {
+                    counterFunc();
+                }
+            }
+            if ($("body").find('.enter').hasClass("submit")) return visitURL();
+            if ($("body").find('.enter').hasClass("phoneSection")) {
+                if ($("body").find('.enter').find('#phone').val().length > 0) counterFunc();
+            }
+            
+            if ($("body").find('.enter').hasClass("shortText")) {
+                if ($("body").find('.enter').find('#short').val().length > 0) counterFunc();
+            }
+            if ($("body").find('.enter').hasClass("emailAddress")) {
+                if ($("body").find('.enter').find('#email').val().length > 0) counterFunc();
+            }
+            if ($("body").find('.enter').hasClass("websiteURL")) {
+                if ($("body").find('.enter').find('#url').val().length > 0) counterFunc();
+            }
+            if ($("body").find('.enter').hasClass("longText")) {
+                if ($("body").find('.enter').find('#long').val().length > 0) counterFunc();
             }
             $("body").find('.enter').next().addClass("enter d-block animate__animated animate__slideInUp");
             $("body").find('.enter').first().removeClass("enter").addClass("d-noneN");
-           
+
         });
 
         $('body').on("keyup", (e) => {
             if (e.keyCode == 13) {
-                
-                if($("body").find('.enter').hasClass("startSection")) {
+
+                if ($("body").find('.enter').hasClass("startSection")) {
                     $(".footer-pw").addClass('d-flex animate__animated animate__fadeIn');
                 }
-                if($("body").find('.enter').hasClass("la") || $("body").find('.enter').hasClass("yesNo")) {
-                    if($("body").find('.enter').find(".global-header-ul-li-required").length > 0) {
+                if ($("body").find('.enter').hasClass("pictureChoice") || $("body").find('.enter').hasClass("la") || $("body").find('.enter').hasClass("yesNo")) {
+                    if ($("body").find('.enter').find(".global-header-ul-li-required").length > 0) {
                         counterFunc();
-                    } 
+                    }
                 }
-                if($("body").find('.enter').hasClass("phoneSection")) {
-                    if($("body").find('.enter').find('#phone').val().length > 0) counterFunc();
+                if ($("body").find('.enter').hasClass("opinionScale")) {
+                    if ($("body").find('.enter').find(".opinion-sacle-span-required").length > 0) {
+                        counterFunc();
+                    }
                 }
-                if($("body").find('.enter').hasClass("shortText")) {
-                    if($("body").find('.enter').find('#short').val().length > 0)  counterFunc();
+                if ($("body").find('.enter').hasClass("phoneSection")) {
+                    if ($("body").find('.enter').find('#phone').val().length > 0) counterFunc();
                 }
-                if($("body").find('.enter').hasClass("longText")) {
-                    if($("body").find('.enter').find('#long').val().length > 0)  counterFunc();
+                if ($("body").find('.enter').hasClass("shortText")) {
+                    if ($("body").find('.enter').find('#short').val().length > 0) counterFunc();
                 }
-               if($("body").find('.enter').hasClass("submit")) return visitURL();
-               if($("body").find('.enter').hasClass("yesNo")) {
+                if ($("body").find('.enter').hasClass("emailAddress")) {
+                    if ($("body").find('.enter').find('#email').val().length > 0) counterFunc();
+                }
+                if ($("body").find('.enter').hasClass("websiteURL")) {
+                    if ($("body").find('.enter').find('#url').val().length > 0) counterFunc();
+                }
+                if ($("body").find('.enter').hasClass("longText")) {
+                    if ($("body").find('.enter').find('#long').val().length > 0) counterFunc();
+                }
+                if ($("body").find('.enter').hasClass("submit")) return visitURL();
+                if ($("body").find('.enter').hasClass("starRating")) return;
+                if ($("body").find('.enter').hasClass("yesNo")) {
                     $(".footer-pw").addClass('animate__animated animate__fadeOut');
                     $(".footer-create").addClass('d-flex animate__animated animate__fadeIn');
-               }
-               $("body").find('.enter').next().addClass("enter d-block animate__animated animate__slideInUp");
-               $("body").find('.enter').first().removeClass("enter").addClass("d-noneN");
-             
+                }
+                $("body").find('.enter').next().addClass("enter d-block animate__animated animate__slideInUp");
+                $("body").find('.enter').first().removeClass("enter").addClass("d-noneN");
+
             }
         });
 
@@ -86,6 +109,18 @@ $(() => {
         })
 
         const globalHeaderUlLi = $(".global-header-ul-li");
+        const divPic = $(".div-pic");
+        divPic.mouseover((e) => {
+            $(e.target).find(".global-header-ul-li-span-key").addClass("d-block");
+        })
+        divPic.mouseleave((e) => {
+            $(e.target).find(".global-header-ul-li-span-key").removeClass("d-block");
+        })
+        divPic.click((e) => {
+            divPic.removeClass("animate__animated animate__bounceIn global-header-ul-li-required");
+            $(e.target).addClass("animate__animated animate__bounceIn global-header-ul-li-required");
+            $(e.target.parentElement.parentElement).next().addClass("d-block");
+        });
         globalHeaderUlLi.mouseover((e) => {
             $(e.target).find(".global-header-ul-li-span-key").addClass("d-block");
         })
@@ -97,31 +132,57 @@ $(() => {
             $(e.target).addClass("animate__animated animate__bounceIn global-header-ul-li-required");
             $(e.target.parentElement).children().last().addClass("d-block");
         });
-        // $(".global-header-ul-li-input").click((e) => {
-        //     $(e.target).addClass("changeInputClass");
-        // })
         $(".global-header-ul-li-input").keyup((e) => {
+            $(e.target.parentElement.parentElement).children().last().addClass("d-block");
+        })
+        $(".global-header-ul-li-email").keyup((e) => {
+            $(e.target.parentElement.parentElement).children().last().addClass("d-block");
+        })
+        $(".global-header-ul-li-url").keyup((e) => {
             $(e.target.parentElement.parentElement).children().last().addClass("d-block");
         })
         $(".global-header-ul-li-txtArea").keyup((e) => {
             $(e.target.parentElement.parentElement).children().last().addClass("d-block");
         })
-
-      
+        $(".global-header-ul-li-number").keyup((e) => {
+            $(e.target.parentElement.parentElement).children().last().addClass("d-block");
+        })
+        $('.opinion-scale-span').click((e) => {
+            $('.opinion-scale-span').removeClass("animate__animated animate__bounceIn opinion-sacle-span-required");
+            $(e.target).addClass("animate__animated animate__bounceIn opinion-sacle-span-required");
+            $(e.target.parentElement).next().addClass("d-block");
+        })
+        $(".start-rating").starRating({
+            totalStars: 5,
+            starSize: 60,
+            emptyColor: 'transparent',
+            hoverColor: 'rgb(95 163 255)',
+            activeColor: 'rgb(31, 127, 255)',
+            ratedColor: '#000',
+            strokeColor: 'rgb(31, 127, 255)',
+            initialRating: 3,
+            ratedColors: ['rgb(31, 127, 255)', 'rgb(31, 127, 255)', 'rgb(31, 127, 255)', 'rgb(31, 127, 255)', 'rgb(31, 127, 255)'],
+            useGradient: false,
+            callback: () => {
+               counterFunc();
+                $("body").find('.enter').next().addClass("enter d-block animate__animated animate__slideInUp");
+                $("body").find('.enter').first().removeClass("enter").addClass("d-noneN");
+            }
+        });
     }
     const initPhone = () => {
         var input = document.querySelector("#phone");
         window.intlTelInput(input, {
             initialCountry: 'auto',
-            preferredCountries: ['us','gb','br','ru','cn','es','it'],
+            preferredCountries: ['us', 'gb', 'br', 'ru', 'cn', 'es', 'it'],
             autoPlaceholder: 'aggressive',
             utilsScript: "https://cdnjs.cloudflare.com/ajax/libs/intl-tel-input/12.1.6/js/utils.js",
-              geoIpLookup: function(callback) {
+            geoIpLookup: function (callback) {
                 fetch('https://ipinfo.io/json', {
                     cache: 'reload'
                 }).then(response => {
-                    if ( response.ok ) {
-                         return response.json()
+                    if (response.ok) {
+                        return response.json()
                     }
                     throw new Error('Failed: ' + response.status)
                 }).then(ipjson => {
@@ -132,6 +193,7 @@ $(() => {
             }
         });
     }
+
 
     const initGlobal = () => {
         toConsole();
